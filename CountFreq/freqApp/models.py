e7c6806caa4f7111f47536c9bcf0ded004e5d84b
sanjayraokadali <raosanjayraokadali@users.noreply.github.com>
2020-12-07 04:34:50 +0000

    Developed Logic for Word count for fresh URls

--- CountFreq/freqApp/models.py
+++ CountFreq/freqApp/models.py
@@ -5,7 +5,45 @@ class UsedURL(models.Model):
 
     url = models.CharField(max_length=500)
 
-    result = models.TextField()
+    word1 =  models.CharField(max_length=100)
+
+    word1count = models.IntegerField()
+
+    word2 = models.CharField(max_length=100)
+
+    word2count = models.IntegerField()
+
+    word3 = models.CharField(max_length=100)
+
+    word3count = models.IntegerField()
+
+    word4 =  models.CharField(max_length=100)
+
+    word4count = models.IntegerField()
+
+    word5 = models.CharField(max_length=100)
+
+    word5count = models.IntegerField()
+
+    word6 = models.CharField(max_length=100)
+
+    word6count = models.IntegerField()
+
+    word7 =  models.CharField(max_length=100)
+
+    word7count = models.IntegerField()
+
+    word8 = models.CharField(max_length=100)
+
+    word8count = models.IntegerField()
+
+    word9 = models.CharField(max_length=100)
+
+    word9count = models.IntegerField()
+
+    word10 = models.CharField(max_length=100)
+
+    word10count = models.IntegerField()
 
     def __str__(self):
 
--- CountFreq/freqApp/views.py
+++ CountFreq/freqApp/views.py
@@ -5,6 +5,7 @@ import trafilatura
 from collections import Counter
 from operator import itemgetter
 from freqApp.models import UsedURL
+import string
 
 
 
@@ -15,13 +16,16 @@ def BasePage(request):
 
 def FrequencyPage(request):
 
-
     return render(request,'freqApp/FrequencyPage.html')
 
 def ResultsPage(request):
+
     url =''
+    text=''
 
     word_pair=[]
+
+    blacklist = []
 
     used_url = UsedURL()
 
@@ -31,24 +35,70 @@ def ResultsPage(request):
 
         if UsedURL.objects.filter(url = url).count() <= 0 and (UsedURL.objects.count() >= 0):
 
-            print(UsedURL.objects.filter(url = url).count())
             down = trafilatura.fetch_url(url)
-            text = trafilatura.extract(down)
+            temp_text = trafilatura.extract(down)
+
+            punc = string.punctuation
+
+            punc+= ''
+
+            for alpha in temp_text:
+
+                if alpha in punc:
+                    pass
+                else:
+                    text += alpha
 
             text = text.split(' ')
             unique_words = sorted(set(text))
 
+            unique_words = [x.lower() for x in unique_words]
+
+            unique_words = sorted(set(unique_words))
+
+            fp = open('1-1000.txt','r')
+
+            for line in fp:
+
+                blacklist.append(line[:len(line)-1])
+
+            fp.close()
 
             for word in unique_words:
 
-                word_pair.append((word,text.count(word)))
+                if word not in blacklist:
+                    word_pair.append((word,text.count(word)))
 
             word_pair = sorted(word_pair,key=itemgetter(1), reverse=True)
 
-            used_url = UsedURL.objects.create(url = url, result = word_pair[:10])
+
+            used_url = UsedURL.objects.create(url = url,
+
+            word1 = word_pair[0][0],
+            word1count = word_pair[0][1],
+            word2 = word_pair[1][0],
+            word2count = word_pair[1][1],
+            word3 = word_pair[2][0],
+            word3count = word_pair[2][1],
+            word4 = word_pair[3][0],
+            word4count = word_pair[3][1],
+            word5 = word_pair[4][0],
+            word5count = word_pair[4][1],
+            word6 = word_pair[5][0],
+            word6count = word_pair[5][1],
+            word7 = word_pair[6][0],
+            word7count = word_pair[6][1],
+            word8 = word_pair[7][0],
+            word8count = word_pair[7][1],
+            word9 = word_pair[8][0],
+            word9count = word_pair[8][1],
+            word10 = word_pair[9][0],
+            word10count = word_pair[9][1],
+
+
+            )
 
             used_url.save()
-
 
         else:
 
